f7aeaf3962fa74663a2da472920e8815b4d5d640
upisdown0206 <upisdown0206@gmail.com>
2021-05-11 22:27:01 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://root:root@reactproject.j3lhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify:false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hellow World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
